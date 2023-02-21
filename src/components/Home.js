b855b7eb58f99e388620ff8b233423c6c5a28865
Chris Mann <chrispmann@gmail.com>
2023-02-21 11:50:54 -0800

    initial commit; basic views created

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import '../App.css';
+import './Home.css'
+
+function Home(){
+
+    return(
+        <div>
+            <p>Home</p>
+        </div>
+    )
+
+}
+
+export default Home
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import '../App.css'
+import React, {useState} from 'react';
+import './NavBar.css';
+
+function NavBar(){
+
+    return(
+        <div>
+            <h1>Serving Calculator</h1>
+        </div>
+    )
+}
+
+export default NavBar
